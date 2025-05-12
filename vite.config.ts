b8a322d5b7ed60a2ec21681de58e94917e55ee6d
JRFelkins <jrfelkins@gmail.com>
2025-05-12 18:15:42 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import json from '@rollup/plugin-json';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    json()
+  ],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react']
   },
+  assetsInclude: ['**/*.jsonl'],
 });
