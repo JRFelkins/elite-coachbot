51d4a464dffbb5f25008e95f5d972257bfe84bfb
JRFelkins <jrfelkins@gmail.com>
2025-05-12 19:51:55 -0500

    Remove JSON plugin to fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import json from '@rollup/plugin-json';
 
 export default defineConfig({
   plugins: [
-    react(),
-    json()
+    react()
   ],
   optimizeDeps: {
     exclude: ['lucide-react']
